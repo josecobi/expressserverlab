94320dd65dc3f0f3141f26a3dbf22f29d1040d31
josecobi <58313777+josecobi@users.noreply.github.com>
2024-01-18 10:35:19 -0500

    defined server called 'dogs'

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// import filesystem module
+const fs = require(fs);
 
+// define engine
+app.engine("dogs", (filePath, options, callback) => {
+    fs.readFile(filePath, (err, content) => {
+      if (err) return callback(err);
 
+      const rendered = content.toString().replaceAll("#title", `${options.title}`).replace("#content", `${options.content}`);
+      return callback(null, rendered);
+    })
+})
+
+app.get("/", (req, res) => {
+    res.send("Server is working");
+})
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}.`);
